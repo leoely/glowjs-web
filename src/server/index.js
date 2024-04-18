d08003c75663cf6593917bb13a87b07d9ad459c0
Leo Brad <>
2024-04-18 22:25:46 +0800

    [Add] Add server part.

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+import https from 'https';
+import fs from 'fs';
+import { CommonHttp, } from 'manner.js/server';
+
+https.createServer({
+  key: fs.readFileSync('asset/glowjs-web-key.pem'),
+  cert: fs.readFileSync('asset/glowjs-web-cert.pem'),
+}, async (req, res) => {
+  const commonHttp = new CommonHttp({
+    title: 'Glow.js',
+    content: 'Glow.js document website.',
+    icon: 'favicon-16x16.png',
+    fonts: ['ttf'],
+    develope: true,
+  });
+  await commonHttp.process(req, res);
+}).listen(8000);
