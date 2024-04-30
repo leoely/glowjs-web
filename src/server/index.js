f7e743c94eb6d6aec642ae66428c3ad706ae0109
Leo Brad <>
2024-04-30 17:19:11 +0800

    [Bugfix] Fix commonHttp logic.

--- src/server/index.js
+++ src/server/index.js
@@ -2,16 +2,17 @@ import https from 'https';
 import fs from 'fs';
 import { CommonHttp, } from 'manner.js/server';
 
+const commonHttp = new CommonHttp({
+  title: 'Glow.js',
+  content: 'Glow.js document website.',
+  icon: 'favicon-16x16.png',
+  fonts: ['ttf'],
+  develope: false,
+});
+
 https.createServer({
   key: fs.readFileSync('asset/glowjs-web-key.pem'),
   cert: fs.readFileSync('asset/glowjs-web-cert.pem'),
 }, async (req, res) => {
-  const commonHttp = new CommonHttp({
-    title: 'Glow.js',
-    content: 'Glow.js document website.',
-    icon: 'favicon-16x16.png',
-    fonts: ['ttf'],
-    develope: false,
-  });
   await commonHttp.process(req, res);
 }).listen(8000);
